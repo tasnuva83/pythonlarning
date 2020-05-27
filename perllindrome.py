717dc53ec3db1235284c29d1364b6f1e409193de
tasnuva83 <tasnuvamishu@gmail.com>
2020-05-27 15:52:32 +0200

    parlindrome

--- perllindrome.py
+++ perllindrome.py
@@ -1,5 +1,11 @@
 st="bangladesh"
 print(st[::-1])
 st1=st[::-1]
-print(st is st1)
+print(st==st1)
+print (st is st1)
 
+my_str="malayalam"
+print(my_str)
+print(my_str[::-1])
+rev_str=my_str[::-1]
+print(my_str==rev_str)
